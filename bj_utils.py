fe7de30d494eae7a6f458d46b0412fdfe91c88ff
prestondntran <prestondnt@gmail.com>
2025-02-28 20:44:34 -0800

    Added documentation to state and bj_utils

--- bj_utils.py
+++ bj_utils.py
@@ -1,4 +1,13 @@
 def score(hand):
+    """
+    Calculates the score of the given hand.
+
+    Args:
+        hand (list[int]): The hand whose score needs to be calculated.
+
+    Returns:
+        tuple: The hand's score and the number of aces in that hand.
+    """
     ace = 0
     sc = 0
     for card in hand:
