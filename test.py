e82a2ff6a0055bd3fcf3fb89c297b1be9ede0a25
VinnieMuffinMan <vincentnguyen3516@gmail.com>
2025-03-14 01:06:50 -0700

    added test cases

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import unittest
+from wordle_utils import get_feedback
+
+
+class TestGetFeedback(unittest.TestCase):
+    def test(self):
+        self.assertEqual(get_feedback("apple", "apple"), [2, 2, 2, 2, 2])
+        self.assertEqual(get_feedback("abcde", "vwxyz"), [0, 0, 0, 0, 0])
+        self.assertEqual(get_feedback("apple", "apron"), [2, 2, 0, 0, 0])
+        self.assertEqual(get_feedback("paper", "apple"), [1, 1, 2, 1, 0])
+        self.assertEqual(get_feedback("allee", "lemon"), [0, 1, 0, 1, 0])
+        self.assertEqual(get_feedback("crane", "candy"), [2, 0, 1, 1, 0])
 
 
 if __name__ == "__main__":
